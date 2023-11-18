485a0baac6f82aac871f15d2e7641b2c08d1e9a6
aluno <aluno@B25-19>
2023-11-18 16:55:17 -0300

    Adicionar arquivos de projeto.

---
+++ banco/Funcionario.cs
@@ -0,0 +1,76 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banco
+{
+    internal class Funcionario:Conexao
+    {
+        public int id;
+        public int id_empresa;
+        public int nivel;
+        public string nome;
+        public string email;
+        public string senha;
+
+        public DateTime data_cadastro;
+
+        public List<Funcionario> GetListaClientes()
+        {
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            try
+            {
+                OpenConnection();
+
+                string query = "SELECT * FROM funcionarios";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+
+
+                            Funcionario novoFuncionario = new Funcionario();
+
+                            novoFuncionario.id = Convert.ToInt32(reader.GetString("id"));
+                            novoFuncionario.id_empresa = Convert.ToInt32(reader.GetString("id_empresa"));
+                            novoFuncionario.nivel = Convert.ToInt32(reader.GetString("nivel"));
+
+                            novoFuncionario.nome = reader.GetString("nome");
+                            novoFuncionario.email = reader.GetString("email");
+                            novoFuncionario.senha = reader.GetString("senha");
+
+                            novoFuncionario.data_cadastro = DateTime.Parse(reader.GetString("data_cadastro"));
+
+
+                            funcionarios.Add(novoFuncionario);
+                        }
+                    }
+                }
+
+                CloseConnection();
+
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+
+            return funcionarios;
+        }
+
+
+
+
+    }
+}
---
+++ banco/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Funcionario funcionarios = new Funcionario();
+
+
+            foreach (Funcionario funcionario in funcionarios.GetListaClientes()) 
+            {
+
+                Console.WriteLine("ID : " + funcionario.id);
+                Console.WriteLine("ID_EMPRESA: " + funcionario.id_empresa);
+                Console.WriteLine("NIVEL : " + funcionario.nivel);
+
+                Console.WriteLine("NOME : " + funcionario.nome);
+                Console.WriteLine("EMAIL : " + funcionario.email);
+                Console.WriteLine("SENHA : " + funcionario.senha);
+                Console.WriteLine("DATA DE CADASTRO : " + funcionario.data_cadastro);
+
+            }
+                Console.ReadLine();
+
+
+        }
+    }
+}
+
+
+
+//Console.WriteLine("Digite o insumo de numero [i]"); //vai ter textbox pra isso talvez não precise.
+//numeroDeInsumos = Convert.ToDouble(Console.ReadLine());
+
+//
+//     ____________
+//    / /|      |\ \  
+//   { }|/o v o\|{ }
+
+//
+
+
